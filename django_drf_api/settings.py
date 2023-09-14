a98d3a7116d92a4907b05c0b2dfefde00ef6d6f9
Myekman <my@traningstemplet.se>
2023-09-14 09:12:31 +0000

    change allowed hosts

--- django_drf_api/settings.py
+++ django_drf_api/settings.py
@@ -72,7 +72,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 # DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io', 'django-rest-framework3-cd622c575206.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io', 'django-rest-framework3-cd622c575206.herokuapp.com']
 
 
 # Application definition
