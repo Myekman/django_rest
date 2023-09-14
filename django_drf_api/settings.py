23de0b91d6c52590f61a13f6c128307e137cf168
Myekman <my@traningstemplet.se>
2023-09-14 11:21:36 +0000

    ..

--- django_drf_api/settings.py
+++ django_drf_api/settings.py
@@ -73,11 +73,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 # DEBUG = 'DEV' in os.environ
 
-# ALLOWED_HOSTS = ['8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io', 'django-rest-framework3-cd622c575206.herokuapp.com']
-ALLOWED_HOSTS = [
-   os.environ.get('ALLOWED_HOSTS'),
-   '8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io',
-]
+ALLOWED_HOSTS = ['8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io', 'django-rest-framework3-cd622c575206.herokuapp.com']
+# ALLOWED_HOSTS = [
+#    os.environ.get('ALLOWED_HOSTS'),
+#    '8000-myekman-djangorest-wrv9tc7u1ap.ws-eu104.gitpod.io',
+# ]
 
 # Application definition
 
