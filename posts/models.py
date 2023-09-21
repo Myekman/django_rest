41a025cadcbfc3ffaed13690c4bf727d0b64f054
Myekman <my@traningstemplet.se>
2023-09-21 09:50:51 +0000

    images change

--- posts/models.py
+++ posts/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_bhoztj', blank=True
+        upload_to='images/', default='../default_post_cb3u4j', blank=True
     )
 
     class Meta:
