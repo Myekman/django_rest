d4408fe8ca150fbbc3299a4fe47c36f599011334
Myekman <my@traningstemplet.se>
2023-09-25 08:19:25 +0000

    api view

--- django_drf_api/views.py
+++ django_drf_api/views.py
@@ -22,7 +22,7 @@ def logout_route(request):
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
-        secure= JWT_AUTH_SECURE,
+        secure=JWT_AUTH_SECURE,
     )
     response.set_cookie(
         key=JWT_AUTH_REFRESH_COOKIE,
@@ -31,6 +31,6 @@ def logout_route(request):
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
         samesite=JWT_AUTH_SAMESITE,
-        secure= JWT_AUTH_SECURE,
+        secure=JWT_AUTH_SECURE,
     )
     return response
